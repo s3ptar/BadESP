1bc5597228214b71b8ae11e988b9941d3bb56c33
s3ptar <rene_graeber@graeber-elektronik.de>
2022-08-22 18:30:14 +0200

    2

--- WebRadio2/src/main.cpp
+++ WebRadio2/src/main.cpp
@@ -1,9 +1,29 @@
+/***********************************************************************
+*! \file:                   FT800_HAL.c
+*  \projekt:                FT800_ESP
+*  \created on:             07.03.2019
+*  \author:                 R. Gräber
+*  \version:                0
+*  \history:                -
+*  \brief
+***********************************************************************/
+ 
+/***********************************************************************
+* Includes
+***********************************************************************/
 #include "Arduino.h"
 #include <SPI.h>
 #include <WiFi.h>
 #include "vs1053_ext.h"
 
-// Digital I/O used
+/***********************************************************************
+* Informations
+***********************************************************************/
+//https://thingpulse.com/esp32-logging/
+/***********************************************************************
+* Declarations
+***********************************************************************/
+ // Digital I/O used
 #define VS1053_CS      2
 #define VS1053_DCS     4
 #define VS1053_DREQ   36
@@ -15,21 +35,47 @@
 String ssid =     "chilihotdog";
 String password = "bxJHckMMkGqEPfY3Jf3nZnAn5FtGYwKZSkzVvbzFHNbpUZfv79GXm8afDuNu";
 
-int volume=5;
+int volume=10;
 
 VS1053 mp3(VS1053_CS, VS1053_DCS, VS1053_DREQ, VSPI, VS1053_MOSI, VS1053_MISO, VS1053_SCK);
 
-//The setup function is called once at startup of the sketch
+/***********************************************************************
+* Global Variable
+***********************************************************************/
+ 
+/***********************************************************************
+* Constant
+***********************************************************************/
+ 
+/***********************************************************************
+* Local Funtions
+***********************************************************************/
+
+
+
+
+/***********************************************************************
+*! \fn          void setup() 
+*  \brief       The setup function is called once at startup of the sketch
+*  \param       none
+*  \exception   none
+*  \return      none
+***********************************************************************/
 void setup() {
 
     Serial.begin(115200);
     SPI.begin();
     //SD.begin();
+
     WiFi.disconnect();
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid.c_str(), password.c_str());
     while (WiFi.status() != WL_CONNECTED) delay(1500);
     Serial.println(WiFi.localIP());
+    ESP_LOGE();
+
+
+
     mp3.begin();
     mp3.setVolume(volume);
     mp3.connecttohost("https://www.ndr.de/resources/metadaten/audio/m3u/ndr2_mv.m3u");
@@ -43,7 +89,14 @@ void setup() {
 	//mp3.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut Schafe stehlen.", "de");
 }
 
-// The loop function is called in an endless loop
+/***********************************************************************
+*! \fn          void loop() 
+*  \brief       The loop function is called in an endless loop
+*  \param       none
+*  \exception   none
+*  \return      none
+***********************************************************************/
+
 void loop()
 {
     mp3.loop();
